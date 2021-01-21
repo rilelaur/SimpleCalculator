56df6fdc205546a7d99b0d7a04a4b4d3da0f00dd
Laura <laurakriley01@gmail.com>
2021-01-20 20:13:54 -0700

    Add project files.

---
+++ SimpleCalculator.vb
@@ -0,0 +1,38 @@
+﻿'Laura Riley
+'RCET0265
+'Spring 2021
+'Say My Name
+'
+
+Option Explicit On
+Option Strict On
+
+'The program will ask the user for two numbers and give them the option to
+'compute the sum or the product and then display the answer
+Module SimpleCalculator
+
+    Sub Main()
+        Dim numberOne As Integer
+        Dim numberTwo As Integer
+        'Dim userInput As Integer
+
+        Console.WriteLine("Input a whole number and press 'Enter'" & vbCrLf)
+        numberOne = Console.Read()
+
+        Console.WriteLine("Please input a second number" & vbCrLf)
+        numberTwo = Console.Read()
+
+
+        'Console.WriteLine("Indicate if you want to add(1) or multiply(2) the two numbers by typing '1' or '2'")
+        'userInput = Console.Read()
+
+        'If userInput = 1 Then
+        '    Console.WriteLine(numberOne + numberTwo)
+        'ElseIf userInput = 2 Then
+        '    Console.WriteLine(numberOne * numberTwo)
+        'End If
+
+        Console.Read()
+    End Sub
+
+End Module
